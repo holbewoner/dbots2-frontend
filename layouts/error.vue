d2b55cb21327329ca35886ce792ac372f8619ae5
abalabahaha <hi@abal.moe>
2017-10-02 18:58:49 -0700

    Bot page cleanup and playing with command lists

--- layouts/error.vue
+++ layouts/error.vue
@@ -5,28 +5,24 @@
             <v-toolbar-side-icon @click.stop="sidebar.open = !sidebar.open"></v-toolbar-side-icon>
             <v-toolbar-title>some page title here probably</v-toolbar-title>
         </v-toolbar>
-        <main class="pb-3">
+        <main class="pb-4">
             <v-container fluid>
                 <v-layout row>
                     <v-flex xs12>
-                        <section>
-                            <v-container class="hero">
-                                <div class="text-xs-center">
-                                    <br/> <!--temporary until i figure out how to CSS properly -->
-                                    <br/>
-                                    <br/>
-                                    <br/>
-                                    <h1>{{ error.statusCode }}</h1>
-                                    <p class="headline">{{ error.message }}</p>
-                                    <p v-if="error.statusCode === 404"><v-btn error to="/">Back to the home page</v-btn></p>
-                                </div>
-                            </v-container>
-                        </section>
+                        <div class="text-xs-center">
+                            <br/> <!--temporary until i figure out how to CSS properly -->
+                            <br/>
+                            <br/>
+                            <br/>
+                            <h1>{{ error.statusCode }}</h1>
+                            <p class="headline">{{ error.message }}</p>
+                            <span v-if="error.statusCode === 404"><v-btn error to="/">Back to the home page</v-btn></span>
+                        </div>
                     </v-flex>
                 </v-layout>
             </v-container>
         </main>
-        <footer />
+        <page-footer />
     </v-app>
 </template>
 
--- pages/bots/_id.vue
+++ pages/bots/_id.vue
@@ -1,27 +1,19 @@
 <template>
     <div>
         <v-container>
-            <v-layout row class="pb-3">
-                <v-flex>
+            <v-layout row class="mb-3">
+                <v-flex class="pr-3 flex--no-grow">
+                    <v-avatar size="9rem">
+                        <img src="http://lorempixel.com/256/256/"/>
+                    </v-avatar>
+                </v-flex>
+                <v-flex xs12 md11>
                     <v-layout column>
-                        <v-flex>
-                            <span class="display-2">Pollr</span>
-                            <span class="title grey--text text--darken-2"> by abalabahaha#1234</span>
-                        </v-flex>
-                        <v-flex>
-                            <p>The world's best moderation bot.</p>
-                        </v-flex>
                         <v-flex>
                             <v-layout row wrap>
                                 <v-flex>
-                                    <div class="bottom">
-                                        <v-chip class="blue lighten-3">Open-Source</v-chip>
-                                        <v-chip class="blue-grey lighten-2">
-                                            <div class="avatar count blue-grey lighten-1">1M</div>
-                                            guilds
-                                        </v-chip>
-                                        <v-chip class="green darken-1 white--text">Online</v-chip>
-                                    </div>
+                                    <span class="display-2">Pollr</span>
+                                    <span class="title grey--text text--darken-2"> by abalabahaha#1234</span>
                                 </v-flex>
                                 <v-flex>
                                     <div class="right">
@@ -30,22 +22,59 @@
                                 </v-flex>
                             </v-layout>
                         </v-flex>
+                        <v-flex>
+                            <p>The world's best moderation bot.</p>
+                        </v-flex>
+                        <v-flex>
+                            <div class="bottom">
+                                <v-chip class="blue lighten-3">Open-Source</v-chip>
+                                <v-chip class="blue-grey lighten-2">
+                                    <div class="avatar count blue-grey lighten-1">1M</div>
+                                    guilds
+                                </v-chip>
+                                <v-chip class="green darken-1 white--text">Online</v-chip>
+                            </div>
+                        </v-flex>
                     </v-layout>
                 </v-flex>
-                <v-flex xs12 md1 ml-2>
-                    <v-avatar size="100%">
-                        <img src="http://lorempixel.com/256/256/"/>
-                    </v-avatar>
-                </v-flex>
             </v-layout>
-            <v-divider/>
-            <v-layout row class="pt-3 pb-3">
-                <v-flex>
+            <v-divider class="mb-3" />
+            <v-layout row wrap class="mb-3">
+                <v-flex xs12>
                     <div class="title">Hello there!</div>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </v-flex>
+                <v-flex xs12 md4 v-for="(category, name) in commands" v-bind:key="name" class="px-2">
+                    <v-card>
+                        <v-list two-line>
+                            <v-list-group>
+                                <v-list-tile slot="item" @click="">
+                                    <v-list-tile-action v-if="category.icon">
+                                        <v-icon>{{ category.icon }}</v-icon>
+                                    </v-list-tile-action>
+                                    <v-list-tile-content>
+                                        <v-list-tile-title>{{ name }}</v-list-tile-title>
+                                        <v-list-tile-sub-title v-if="category.description">{{ category.description }}</v-list-tile-sub-title>
+                                    </v-list-tile-content>
+                                    <v-list-tile-action>
+                                        <v-icon>keyboard_arrow_down</v-icon>
+                                    </v-list-tile-action>
+                                </v-list-tile>
+                                <v-list-tile v-for="command in category.commands" v-bind:key="command.name" @click="">
+                                    <v-list-tile-content>
+                                        <v-list-tile-title>{{ prefix + command.name }}</v-list-tile-title>
+                                        <v-list-tile-sub-title v-if="command.description">{{ command.description }}</v-list-tile-sub-title>
+                                    </v-list-tile-content>
+                                    <v-list-tile-action>
+                                        <v-icon>info</v-icon>
+                                    </v-list-tile-action>
+                                </v-list-tile>
+                            </v-list-group>
+                        </v-list>
+                    </v-card>
                 </v-flex>
             </v-layout>
             <v-speed-dial fixed bottom right hover>
@@ -69,7 +98,33 @@ export default {
                 {to: './report', class: 'red darken-2', tooltip: {html: 'Report'}, icon: 'report'},
                 {to: './edit', tooltip: {html: 'Edit'}, icon: 'mode_edit'},
                 {to: './comment', tooltip: {html: 'Review'}, icon: 'mode_comment'}
-            ]
+            ],
+            prefix: "!",
+            commands: {
+                'Fun': {
+                    icon: 'casino',
+                    commands: [
+                        { name: 'roll', description: 'Roll some dice' },
+                        { name: 'unroll', description: 'Unroll some dice' }
+                    ]
+                },
+                'Moderation': {
+                    icon: 'security',
+                    description: 'Empower your mods!',
+                    commands: [
+                        { name: 'ban', description: 'Ban someone' },
+                        { name: 'bun', description: 'Cook a bun' }
+                    ]
+                },
+                'Games': {
+                    icon: 'videogame_asset',
+                    description: 'Distract your mods!',
+                    commands: [
+                        { name: 'guess', description: 'Guess that song!' },
+                        { name: 'count', description: 'Play the counting game!' }
+                    ]
+                }
+            }
         }
     },
     validate ({ params }) {
@@ -89,5 +144,8 @@ export default {
 }
 .speed-dial {
     bottom: 3rem;
+}
+.flex--no-grow {
+    flex-grow: 0;
 }
 </style>
