6629fd00f9eb3ce1b88b3622605d9c794515087c
FiniteReality <digpoealternate@gmail.com>
2017-11-08 18:02:45 +0000

    Rework the bot page so that it makes more sense
    Work in progress

--- pages/bots/_id/index.vue
+++ pages/bots/_id/index.vue
@@ -1,6 +1,6 @@
 <template>
-    <v-container fluid>
-        <v-layout row class="mb-3">
+    <v-container>
+        <v-layout row>
             <v-flex class="pr-3 flex--no-grow">
                 <v-badge color="green lighten-2" class="badge--status">
                     <span slot="badge"></span>
@@ -24,7 +24,6 @@
                     </v-flex>
                     <v-flex>
                         <v-btn append to="invite">Invite</v-btn>
-                        <v-btn append to="stats">Stats</v-btn>
                     </v-flex>
                 </v-layout>
             </v-flex>
@@ -51,50 +50,69 @@
                 </v-layout>
             </v-flex>
         </v-layout>
-        <v-divider class="mb-3" />
-        <v-layout row wrap class="mb-3">
-            <!-- TODO: this should return markdown! -->
-            <v-flex xs12 v-html="parseMarkdown(bot.description)">
-            </v-flex>
-        </v-layout>
-        <v-layout row wrap>
-            <v-flex xs12>
-                <p class="display-2">Commands</p>
-            </v-flex>
+        <v-tabs dark :scrollable="false">
+            <v-tabs-bar>
+                <v-tabs-item ripple href="#info">Info</v-tabs-item>
+                <v-tabs-item ripple href="#commands">Commands</v-tabs-item>
+                <v-tabs-item ripple href="#stats">Stats</v-tabs-item>
+                <v-tabs-slider />
+            </v-tabs-bar>
+            <v-divider class="mb-3" />
+            <v-tabs-items>
+                <v-tabs-content id="info" v-html="parseMarkdown(bot.description)">
+                </v-tabs-content>
+                <v-tabs-content id="commands">
+                    <v-layout row>
+                        <v-flex xs12 md4 v-for="category in commands.categories" v-bind:key="category.name" class="px-2">
+                            <v-card>
+                                <v-list two-line>
+                                    <v-list-group>
+                                        <v-list-tile slot="item">
+                                            <v-list-tile-action v-if="category.icon">
+                                                <v-icon>{{category.icon}}</v-icon>
+                                            </v-list-tile-action>
+                                            <v-list-tile-content>
+                                                <v-list-tile-title>{{category.name}}</v-list-tile-title>
+                                                <v-list-tile-sub-title v-if="category.description">{{category.description}}</v-list-tile-sub-title>
+                                            </v-list-tile-content>
+                                            <v-list-tile-action>
+                                                <v-icon>keyboard_arrow_down</v-icon>
+                                            </v-list-tile-action>
+                                        </v-list-tile>
+                                        <v-list-tile class="list--no-pad-left" v-for="command in category.commands" v-bind:key="command.name">
+                                            <v-list-tile-action v-if="command.icon">
+                                                <v-icon>{{command.icon}}</v-icon>
+                                            </v-list-tile-action>
+                                            <v-list-tile-content>
+                                                <v-list-tile-title>{{activePrefix}}{{command.name}}</v-list-tile-title>
+                                                <v-list-tile-sub-title v-if="command.description">{{command.description}}</v-list-tile-sub-title>
+                                            </v-list-tile-content>
+                                            <v-list-tile-action>
+                                                <v-dialog width="600px">
+                                                    <v-icon class="clickable" slot="activator">info</v-icon>
+                                                    <v-card>
+                                                        <v-card-title>
+                                                            <span class="headline">{{activePrefix}}{{command.name}}</span>
+                                                        </v-card-title>
+                                                        <v-card-text>
+                                                            {{command.description}}
+                                                        </v-card-text>
+                                                    </v-card>
+                                                </v-dialog>
+                                            </v-list-tile-action>
+                                        </v-list-tile>
+                                    </v-list-group>
+                                </v-list>
+                            </v-card>
+                        </v-flex>
+                    </v-layout>
+                </v-tabs-content>
+                <v-tabs-content id="stats">
+                    <p class="display-3">TODO: stats</p>
+                </v-tabs-content>
+            </v-tabs-items>
+        </v-tabs>
 
-            <v-flex xs12 md4 v-for="category in commands.categories" v-bind:key="category.name" class="px-2">
-                <v-card>
-                    <v-list two-line>
-                        <v-list-group>
-                            <v-list-tile slot="item" @click="">
-                                <v-list-tile-action v-if="category.icon">
-                                    <v-icon>{{category.icon}}</v-icon>
-                                </v-list-tile-action>
-                                <v-list-tile-content>
-                                    <v-list-tile-title>{{category.name}}</v-list-tile-title>
-                                    <v-list-tile-sub-title v-if="category.description">{{category.description}}</v-list-tile-sub-title>
-                                </v-list-tile-content>
-                                <v-list-tile-action>
-                                    <v-icon>keyboard_arrow_down</v-icon>
-                                </v-list-tile-action>
-                            </v-list-tile>
-                            <v-list-tile class="list--no-pad-left" v-for="command in category.commands" v-bind:key="command.name" @click="">
-                                <v-list-tile-action v-if="command.icon">
-                                    <v-icon>{{command.icon}}</v-icon>
-                                </v-list-tile-action>
-                                <v-list-tile-content>
-                                    <v-list-tile-title>{{activePrefix}}{{command.name}}</v-list-tile-title>
-                                    <v-list-tile-sub-title v-if="command.description">{{command.description}}</v-list-tile-sub-title>
-                                </v-list-tile-content>
-                                <v-list-tile-action>
-                                    <v-icon>info</v-icon>
-                                </v-list-tile-action>
-                            </v-list-tile>
-                        </v-list-group>
-                    </v-list>
-                </v-card>
-            </v-flex>
-        </v-layout>
         <v-speed-dial fixed bottom right hover>
             <v-btn slot="activator" fab>
                 <v-icon>settings</v-icon>
@@ -142,6 +160,9 @@ export default {
         getTagName(tag) {
             return (tags[tag] || {icon: 'error_outline', name: 'Unknown'}).name
         },
+        displayInfo(command) {
+
+        },
         parseMarkdown(markdown) {
             return marked(markdown)
         }
@@ -167,6 +188,7 @@ export default {
     },
     data() {
         return {
+            scroll: 0,
             links: [
                 {to: 'report', class: 'red darken-2', tooltip: 'Report', icon: 'report'},
                 {to: 'edit', tooltip: 'Edit', icon: 'mode_edit'},
@@ -191,5 +213,8 @@ export default {
     width: auto !important;
 
     padding: 0px 6px;
+}
+i.material-icons.icon.clickable {
+    cursor: pointer;
 }
 </style>
